8994af215f8e991da130cc20ca66b28a751d9113
NDUMAN <nqtembe@gmail.com>
2021-02-15 00:04:02 +0200

    new commit
    format change

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 let btn = document.getElementById('Btn');
 
-btn.addEventListener('click', function(){
+btn.addEventListener('click', function () {
 
-let billAmount = document.getElementById('bill_amt').value;
-let tipPercentage = document.getElementById('tip_perc').value;
+    let billAmount = document.getElementById('bill_amt').value;
+    let tipPercentage = document.getElementById('tip_perc').value;
 
-let tipAmount = document.getElementById('tip_amt').value = billAmount / tipPercentage;
-document.getElementById('tot_bill').value = parseFloat(billAmount) + parseFloat(tipAmount);
+    let tipAmount = document.getElementById('tip_amt').value = billAmount / tipPercentage;
+    document.getElementById('tot_bill').value = parseFloat(billAmount) + parseFloat(tipAmount);
 })
