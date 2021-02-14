4e3440d60380500d9d02ef551d73261835e3b97e
NDUMAN <nqtembe@gmail.com>
2021-02-15 00:02:51 +0200

    first commit
    all the required files

---
+++ main.js
@@ -0,0 +1,10 @@
+let btn = document.getElementById('Btn');
+
+btn.addEventListener('click', function(){
+
+let billAmount = document.getElementById('bill_amt').value;
+let tipPercentage = document.getElementById('tip_perc').value;
+
+let tipAmount = document.getElementById('tip_amt').value = billAmount / tipPercentage;
+document.getElementById('tot_bill').value = parseFloat(billAmount) + parseFloat(tipAmount);
+})
